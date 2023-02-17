da91ae8a423ea6e5e897ab54359f4d407ce81d57
Admin <makwanadharmishtha44@gmail.com>
2023-02-17 18:50:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:combo_1/homescreen.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context)=>Photo1(),
+      },
+    ),
+  );
+}
